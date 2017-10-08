11ef6b483c2ac53108d8dff6f27df1f9b8d87eed
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-10-08 16:26:48 -0400

    Updated for KSP 1.3.1

--- Hooligan_Labs/deploy.bat
+++ Hooligan_Labs/deploy.bat
@@ -1,6 +1,6 @@
 
 @echo off
-set H=R:\KSP_1.3.0_dev
+set H=R:\KSP_1.3.1_dev
 echo %H%
 cd
 
@@ -8,5 +8,5 @@ copy /Y "AugmentedReality\bin\Debug\AugmentedReality.dll" "GameData\AugmentedRea
 copy /Y AugmentedReality.version GameData\AugmentedReality
 
 cd GameData
-xcopy /y /s AugmentedReality "%H%\GameData\AugmentedReality"
+xcopy /y /s /i AugmentedReality "%H%\GameData\AugmentedReality"
 
